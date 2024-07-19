d779030940852b0b332d1aa41f1efcf9d8f4004c
devpr <devpratasingh122@gmail.com>
2024-07-19 13:02:11 +0530

    changed recoilsetstate

--- frontend/leetcode/src/components/topbar.tsx
+++ frontend/leetcode/src/components/topbar.tsx
@@ -1,4 +1,4 @@
-import { useRecoilState, useRecoilValue } from "recoil"
+import { useRecoilState } from "recoil"
 
 import  {isauthenticated} from "../../store/recoil"
 import { Link, useNavigate } from "react-router-dom";

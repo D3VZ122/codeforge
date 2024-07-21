c22c7f3069e30d64fa9b4b5a72f23b2f6b97d862
devpr <devpratasingh122@gmail.com>
2024-07-21 14:34:42 +0530

    added deb

--- frontend/leetcode/src/pages/problemSingle.tsx
+++ frontend/leetcode/src/pages/problemSingle.tsx
@@ -28,7 +28,6 @@ export default function ProblemSingle() {
     status:"wrong",
     testcases:""
   });
-  {console.log(output);}
   useEffect(() => {
     const getData = async () => {
       const resp = await axios.get(server + "/api/v1/problem/" + id);
@@ -127,12 +126,11 @@ target = 9
           </div>
         </div>
         <div className="mt-6">
-              <h2 className="text-lg font-medium mb-2">Result</h2>
-              <div className="bg-gray-900 text-white rounded-lg p-4 h-32 overflow-auto">
-                <pre className="whitespace-pre-wrap" />
-             
-              </div>
-            </div>
+        <h2 className="text-lg font-medium mb-2">Result</h2>
+       <div className="bg-gray-900 text-white rounded-lg p-4 h-32 overflow-auto">
+      <pre className="whitespace-pre-wrap">{output.status ? output.status : "No status available"}</pre>
+     </div>
+    </div>
       </div>
     </div>
   );
